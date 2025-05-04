ec74d9635d7cec8d625c676df0eb4f870c40de48
Jialiang Cao <jcao2080@gmail.com>
2025-05-04 11:18:47 -0400

    Fix

--- app.py
+++ app.py
@@ -104,6 +104,9 @@ def tts():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
 # == Example Run ==
 #   "TrafficPrediction": [
 #     {
